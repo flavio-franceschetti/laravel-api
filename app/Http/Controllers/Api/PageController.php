3897f810170b9949a2c4dd4cc79e38f445d1848d
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-09-30 15:40:25 +0200

    feat: add technologies to projects API

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -11,7 +11,7 @@ class PageController extends Controller
 
     // creo la funzione index per visualizzare tutti i miei progetti dentro l'api
     public function index(){
-        $projects = Project::orderBy('id', 'desc')->with('type')->get();
+        $projects = Project::orderBy('id', 'desc')->with('type', 'technologies')->get();
 
         //ritorno un file json con il risultato della richiesta e l'array con tutti i progetti
         return response()->json([
